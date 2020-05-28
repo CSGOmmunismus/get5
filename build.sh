b872bb3bd86d8f8ecbe4f9b1109697882dce492c
Marko Valing <marko@radr.eu>
2020-05-28 11:15:58 +0300

    Fix travis and build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" == "beta" ]; then
     smversion scripting/get5/version.sp scripting/get5/manual_version.sp $TRAVIS_BUILD_NUMBER --gitcommit
-    smbuilder
+    smbuilder --compiler=addons/sourcemod/scripting/spcomp
     cd builds/get5/
     zip -r get5.zip *
     git config --local user.name "TRAVIS"
@@ -9,7 +9,7 @@ if [ "$1" == "beta" ]; then
     export TRAVIS_TAG=${TRAVIS_TAG:-$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)}
     git tag beta-$TRAVIS_TAG
 else
-    smbuilder
+    smbuilder --compiler=addons/sourcemod/scripting/spcomp
     cd builds/get5/
     zip -r get5.zip *
 fi
