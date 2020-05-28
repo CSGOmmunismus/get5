e073b7252d92e2d03b43825a7205179f2d31daf1
Marko Valing <marko@radr.eu>
2020-05-28 11:36:14 +0300

    again

--- build.sh
+++ build.sh
@@ -4,13 +4,10 @@ if [ "$1" == "beta" ]; then
     smversion scripting/get5/version.sp scripting/get5/manual_version.sp $TRAVIS_BUILD_NUMBER --gitcommit
     smbuilder --compiler=addons/sourcemod/scripting/spcomp
     cd builds/get5/
-    zip -r get5.zip *
-    git config --local user.name "TRAVIS"
-    git config --local user.email "travis@csgommunismus.com"
+    zip -r get5.zip addons cfg/get5 LICENSE README.md
     export TRAVIS_TAG=${TRAVIS_TAG:-$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)}
-    git tag beta-$TRAVIS_TAG
 else
     smbuilder --compiler=addons/sourcemod/scripting/spcomp
     cd builds/get5/
-    zip -r get5.zip *
+    zip -r get5.zip addons cfg/get5 LICENSE README.md
 fi
